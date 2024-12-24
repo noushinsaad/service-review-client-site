746f51b32acb2fc8fa3fb06ad01fbbe05e862a04
Md. Noushin Islam <noushinshoha@gmail.com>
2024-12-24 19:20:38 +0600

    add footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../shared/NavBar";
+import Footer from "../shared/Footer";
 
 
 const MainLayout = () => {
     return (
-        <div className='max-w-7xl mx-auto'>
+        <div className='bg-gray-100'>
             <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/shared/NavBar.jsx
+++ src/shared/NavBar.jsx
@@ -27,7 +27,7 @@ const NavBar = () => {
     </>
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 px-16">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
